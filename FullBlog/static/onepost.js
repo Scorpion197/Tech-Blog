85d70ba479731a80f1efc12739e5d561675aac2b
Gaouaoui Kamel <doudou.gaw@gmail.com>
2020-12-06 21:27:25 +0100

    Add files via upload

---
+++ FullBlog/static/onepost.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+
+    var webDev = 'crimson';
+    var machineLearning = '#2ecc71';
+    var infoSec = '#3498db';
+    var gameDev = '#f1c40f';
+
+    $('.web-dev').css('background', webDev);
+    $('.game-dev').css('background', gameDev);
+    $('.info-sec').css('background', infoSec);
+    $('.machine-learning').css('background', machineLearning);
+
+});
